2ac376ed896b74bbe47389362095eb2e2ad94e3a
eduardobaratela <eduardo_baratela@usp.br>
2018-04-23 22:50:17 -0300

    Chain codes terminado

--- ChainCodes/src/calculos.java
+++ ChainCodes/src/calculos.java
@@ -5,21 +5,36 @@ import javax.imageio.ImageIO;
 
 public class calculos {
 	
-	private String s;
 	private BufferedImage image;
 	private int maxX, maxY;
-	private int ptoMin[] = new int[2];
+	private int[] ptoMin = new int[2];
+	private int[] ptoMax = new int[2];
+	private int[][] visitado;
+	private double perimetro;
+	private int[] ultimo = new int[2];
 	
-	public void leImagem() throws IOException {	
-		s = "oval.png";
+	public void leImagem(String s) throws IOException {	
 		File inFile = new File(s);
 		image = ImageIO.read(inFile);
+		this.perimetro = 0;
+		
+	}
+	
+	public void zeraVisitado() {
+		
+		this.visitado = new int[this.maxY][this.maxX];
+		
+		for (int i = 0; i < this.maxY; i++) {
+			for (int j = 0; j < this.maxX; j++) {
+				this.visitado[i][j] = 0;
+			}
+		}
 	}
 	
 	public void printa() {
-		for (int i = this.maxX-1;  i >= 0; i--) {
-			for (int j = 0; j < this.maxY; j++) {
-				System.out.print(image.getRGB(i, j));
+		for (int i = 0;  i < getmaxY(); i++) {
+			for (int j = 0; j < getmaxX(); j++) {
+				System.out.print(image.getRGB(j, i));
 			}
 			System.out.println("\n");
 		}
@@ -27,28 +42,29 @@ public class calculos {
 	
 	public int getmaxY() {
 		
-		maxY =  image.getData().getHeight();
-		System.out.println(maxY);
+		maxY =  image.getHeight();
+		
 		return maxY;
 	}
 	
 	public int getmaxX() {
 		
-		this.maxX = image.getData().getWidth();
-		System.out.println(maxX);
+		this.maxX = image.getWidth();
+		
 		return maxX;
 		
 	}
 	
 	public int[] getPtoMin(){
+		boolean flag = false;
 		this.maxX = getmaxX();
 		this.maxY = getmaxY();
-		for (int i = this.maxX-1;  i >= 0; i--) {
-			for (int j = 0; j < this.maxY; j++) {
-				if(image.getRGB(i, j) != -1) {
+		for (int i = 0;  i <this.maxY; i++) {
+			for (int j = 0; j < this.maxX; j++) {
+				if(image.getRGB(j, i) != -1 && !(flag)) {
 					this.ptoMin[0] = i;
 					this.ptoMin[1] = j;
-					break;
+					flag = true;
 				}
 			}
 		}
@@ -56,26 +72,32 @@ public class calculos {
 		return ptoMin;
 	}
 	
-	public int getAltura() {
-		int height = 0, cont = 0;
+	public int[] getPtoMax(){
+		boolean flag = false;
 		this.maxX = getmaxX();
 		this.maxY = getmaxY();
-		for (int i = ptoMin[0]; i < this.maxX; i++) {
-			for (int j = 0; j < this.maxY; j++) {
-				if(image.getRGB(i, j) != -1)
-					cont++;
+		for (int i = this.maxY-1;  i >= 0; i--) {
+			for (int j = this.maxX-1;  j >= 0; j--) {
+				if(image.getRGB(j, i) != -1 && !(flag)) {
+					this.ptoMax[0] = i;
+					this.ptoMax[1] = j;
+					flag = true;
+				}
 			}
-			if(cont > height)
-				height = cont;
-			cont = 0;
 		}
+		
+		return ptoMax;
+	}
+	
+	public int getAltura() {
+		int height = this.ptoMax[0] - this.ptoMin[0] + 1;
 		return height;
 	}
 	
 	public int getLargura() {
 		int width = 0, cont = 0;
 		
-		for (int i = 0; i < this.maxY; i++) {
+		for (int i = this.ptoMin[0]; i < this.ptoMax[0]; i++) {
 			for (int j = 0; j < this.maxX; j++) {
 				if(image.getRGB(j, i) != -1)
 					cont++;
@@ -85,6 +107,132 @@ public class calculos {
 			cont = 0;
 		}
 		return width;
+	}
+	
+	public boolean checaBorda(int i, int j) {
+		
+		if (image.getRGB(j, i) == -1) return false;
+		
+		if(image.getRGB(j-1, i) == -1)  //esquerda
+			return true;
+		if(image.getRGB(j+1, i) == -1)  //direita
+			return true;
+		if(image.getRGB(j, i-1) == -1)  //cima
+			return true;
+		if(image.getRGB(j, i+1) == -1)  //baixo
+			return true;
+		
+		return false;
+	}
+	
+	public int borda() {
+		int edge = 0;
+		
+		for (int i = this.ptoMin[0]; i <= this.ptoMax[0]; i++) {
+			for (int j = 0; j < this.maxX; j++) {
+				if(image.getRGB(j, i) != -1)
+					if(checaBorda(i, j)) edge++;
+			}
+		}
+		
+		return edge;
+	}
+	
+	public int[] achaVizinho(int i, int j) {
+		int aux[] = new int[2];	//aux[0] = i   / aux[1] = j
+		
+		//norte
+		if(checaBorda(i-1, j) && visitado[i-1][j] == 0) {
+			i--;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += 1;
+    		return aux;
+		}
+		//nordeste
+		if(checaBorda(i-1, j+1) && visitado[i-1][j+1] == 0) {
+			i--;
+			j++;
+			aux[0] = i;
+		    aux[1] = j;
+		    this.perimetro += Math.sqrt(2);
+		    return aux;
+		}
+		//leste
+		if(checaBorda(i, j+1) && visitado[i][j+1] == 0) {
+			j++;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += 1;
+    		return aux;
+		}
+		//sudeste
+		if(checaBorda(i+1, j+1) && visitado[i+1][j+1] == 0) {
+			i++;
+			j++;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += Math.sqrt(2);
+    		return aux;
+		}
+		//sul
+		if(checaBorda(i+1, j) && visitado[i+1][j] == 0) {
+			i++;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += 1;
+    		return aux;
+		}
+		//sudoeste
+		if(checaBorda(i+1, j-1) && visitado[i+1][j-1] == 0) {
+			i++;
+			j--;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += Math.sqrt(2);
+    		return aux;
+		}
+		//oeste
+		if(checaBorda(i, j-1) && visitado[i][j-1] == 0) {
+			j--;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += 1;
+    		return aux;
+		}
+		//noroeste
+		if(checaBorda(i-1, j-1) && visitado[i-1][j-1] == 0) {
+			i--;
+			j--;
+			aux[0] = i;
+    		aux[1] = j;
+    		this.perimetro += Math.sqrt(2);
+    		return aux;
+		}
+	
+		this.ultimo [0] = i;
+		this.ultimo[1] = j;
+		aux[0] = i;
+		aux[1] = j;
+    	return aux;
+	}
+	
+	public double calculaPerimetro(int i, int j) {
+		int[] vizinho = new int[2];
+		
+		vizinho = achaVizinho(i, j);
+		this.visitado[i][j] = 1;
+		
+		if (this.visitado[vizinho[0]][vizinho[1]] == 0)
+			calculaPerimetro(vizinho[0], vizinho[1]);		
+		
+		if(i == ptoMin[0] && j == ptoMin[1]) {
+			if(i == this.ultimo[0] || j == this.ultimo[1])
+				this.perimetro++;
+			else this.perimetro += Math.sqrt(2);
+		}
+		
+		return perimetro;
 	}
 	
 	
--- ChainCodes/src/principal.java
+++ ChainCodes/src/principal.java
@@ -5,19 +5,36 @@ public class principal {
 	
 	public static void main(String[] args) throws IOException {
 		calculos m = new calculos();
-		m.leImagem();
 		int[] ptoMin = new int[2];
-		int larg = 0, alt = 0;
 		
+		String s = "circulo.png";
+		m.leImagem(s);
 		ptoMin = m.getPtoMin();
-		alt = m.getLargura();
-		larg = m.getAltura();
+		m.getPtoMax();
 		
-		//m.printa();
+		System.out.println("Circulo");	
+		System.out.println("Inicio: " + ptoMin[1] + " " + ptoMin[0]);
+		System.out.println("Largura: " + m.getLargura());
+		System.out.println("Altura: " + m.getAltura());
+		System.out.println("Número de pontos na borda: " + m.borda());
+		m.zeraVisitado();
+		System.out.println("Perimetro: " + m.calculaPerimetro(ptoMin[0], ptoMin[1]));
 		
-		System.out.println(" 0 " + ptoMin[1] + " 1 " + ptoMin[0]);
-		System.out.println("alt = " + larg);
-		System.out.println("larg = " + alt);
+		System.out.println();
+		System.out.println();
+		
+		s = "oval.png";
+		m.leImagem(s);
+		ptoMin = m.getPtoMin();
+		m.getPtoMax();
+		
+		System.out.println("Oval");	
+		System.out.println("Inicio: " + ptoMin[1] + " " + ptoMin[0]);
+		System.out.println("Largura: " + m.getLargura());
+		System.out.println("Altura: " + m.getAltura());
+		System.out.println("Número de pontos na borda: " + m.borda());
+		m.zeraVisitado();
+		System.out.println("Perimetro: " + m.calculaPerimetro(ptoMin[0], ptoMin[1]));
 	}
 	
 }
